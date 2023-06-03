6061090bbeb193820fedefa5d87174e86e2072c4
Timur <timazizov@gmail.com>
2023-06-03 07:57:24 +0600

    added couple ways to find duplicate

--- src/array/FindDuplicate.java
+++ src/array/FindDuplicate.java
@@ -16,7 +16,7 @@ public class FindDuplicate {
         }
 
         System.out.println(Arrays.toString(nums));
-        System.out.println(findDuplicate(nums));
+        System.out.println(findDuplicateInnerLoop(nums));
     }
 
 
@@ -31,6 +31,30 @@ public class FindDuplicate {
                 return nums[i];
             }
         }
+        return -1;
+    }
+
+    public static int findDuplicateBySorting(int nums[]) {
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+
+    public static int findDuplicateInnerLoop(int nums[]) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+
         return -1;
     }
 }
