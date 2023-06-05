803bd09ca8c40b94337819b81b0f425862500566
Timur <timazizov@gmail.com>
2023-06-05 17:02:41 +0600

    Sign of the product

---
+++ src/array/SignOfTheProduct.java
@@ -0,0 +1,17 @@
+package array;
+
+public class SignOfTheProduct {
+
+    public int arraySign(int[] nums) {
+        int minusCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                return 0;
+            } else if(nums[i] < 0) {
+                minusCount++;
+            }
+        }
+
+        return minusCount % 2 == 0 ? 1 : -1;
+    }
+}
